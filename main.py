7623f12d95ce300fc0e1a998c944d9aacf32837e
Helton Barbosa <heltonbs@gmail.com>
2025-08-08 22:41:37 -0300

    Planejamento da API da Livraria

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return { "message": "Hello World!" }
+# Planejamento da API Livraria
+# / ----------------------------> boas vindas
+# /list-books ------------------> listar todos os livros
+# /list-book-by-index/{index} --> listar um livro
+# /get-random-boom -------------> sugerir um livro aleatório
+# /add-book --------------------> adicionar um novo livro
 
