29ffdace75075e19fb4780fb2792616a5f4a1ee2
Helton Barbosa <heltonbs@gmail.com>
2025-08-08 22:54:44 -0300

    Endpoint /list-books

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOK_DATABASE = [
+    "Programando em Turbo Pascal 7.0",
+    "Absolute FreeBSD - The Complete Guide to FreeBSD",
+    "Aplicações iOS com Lazarus",
+    "Linux A Bíblia",
+    "Think Python",
+    "Robust Python",
+]
+
 # Planejamento da API Livraria
 # / ----------------------------> boas vindas [Endpoint criada]
-# /list-books ------------------> listar todos os livros
+# /list-books ------------------> listar todos os livros [Endpoint criada]
 # /list-book-by-index/{index} --> listar um livro
 # /get-random-boom -------------> sugerir um livro aleatório
 # /add-book --------------------> adicionar um novo livro
@@ -12,4 +21,8 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return "Seja bem-vindo à Livraria!"
+
+@app.get("/list-books")
+async def list_books():
+    return { "books": [BOOK_DATABASE] }
 
