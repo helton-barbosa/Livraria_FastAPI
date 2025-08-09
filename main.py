4ec9dc79a61e3a9f2e35707c95059619cb4a6c70
Helton Barbosa <heltonbs@gmail.com>
2025-08-08 22:45:13 -0300

    Endpoint /

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # Planejamento da API Livraria
-# / ----------------------------> boas vindas
+# / ----------------------------> boas vindas [Endpoint criada]
 # /list-books ------------------> listar todos os livros
 # /list-book-by-index/{index} --> listar um livro
 # /get-random-boom -------------> sugerir um livro aleatório
 # /add-book --------------------> adicionar um novo livro
+
+@app.get("/")
+async def home():
+    return "Seja bem-vindo à Livraria!"
 
